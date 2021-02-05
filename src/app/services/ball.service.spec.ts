9419a42cdc551ee0e4a376ef9f3506fce9b54dc0
mtosheva <70014293+mtosheva@users.noreply.github.com>
2021-02-05 01:08:50 +0100

    added tests for collisions between balls

--- src/app/services/ball.service.spec.ts
+++ src/app/services/ball.service.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { BOUNCE, FRICTION, GRAVITY } from '../app.constants';
 import { Ball2D } from '../models/ball2D.model';
 import { Point } from '../models/point.model';
@@ -162,13 +162,87 @@ describe('BallService', () => {
     let expectedpositionY = ball.point.positionY;
     let expectedpositionX = 100 - ball.radius;
  
-
     service.checkCollisionWithWallsAndUpdateBall(ball,100,100);
 
     expect(ball.speed.vy).toEqual(expectedVy);
     expect(ball.speed.vx).toEqual(expectedVx);
     expect(ball.point.positionY).toEqual(expectedpositionY);  
     expect(ball.point.positionX).toEqual(expectedpositionX);  
+
+  });
+
+  it('checkCollisionsBetweenBallsAndUpdateVelocity should not change velocities if balls are not colliding', () => {
+    spyOn(PhysicsCalculationsService, 'areBallsIntersecting').and.returnValue(false);
+
+    // point values are irelevant for the test
+    let point1 = new Point(10, 10)
+    let ball1 = new Ball2D(point1);
+
+    //new speed is added so we can control the output
+    ball1.speed.vx = 0.5;
+    ball1.speed.vy = 0.5;
+
+    // point values are irelevant for the test
+    let point2 = new Point(10, 10)
+    let ball2 = new Ball2D(point2);
+
+    //new speed is added so we can control the output
+    ball2.speed.vx = 0.1;
+    ball2.speed.vy = 0.1;
+
+
+    let expectedVyBall1 = ball1.speed.vy;
+    let expectedVxBall1 = ball1.speed.vx;
+    let expectedVyBall2 = ball2.speed.vy;
+    let expectedVxBall2 = ball2.speed.vx;
+    let balls: Array<Ball2D> = new Array();
+    balls.push(ball1);
+    balls.push(ball2);
+ 
+    service.checkCollisionsBetweenBallsAndUpdateVelocity(balls);
+
+    expect(balls[0].speed.vy).toEqual(expectedVyBall1);
+    expect(balls[0].speed.vx).toEqual(expectedVxBall1);
+    expect(balls[1].speed.vy).toEqual(expectedVyBall2);  
+    expect(balls[1].speed.vx).toEqual(expectedVxBall2);  
+
+  });
+
+
+  it('checkCollisionsBetweenBallsAndUpdateVelocity should change velocities if balls are colliding', () => {
+    spyOn(PhysicsCalculationsService, 'areBallsIntersecting').and.returnValue(true);
+    spyOn(PhysicsCalculationsService, 'calculateDistanceOfCollidingVector').and.returnValue(20); 
+    spyOn(PhysicsCalculationsService, 'calculateCollisionSpeed').and.returnValue(2); 
+
+    let expectedVyBall1 = 0.23529411764705885;
+    let expectedVxBall1 = 1.1776212618576576;
+    let expectedVyBall2 = 0.69627;
+    let expectedVxBall2 = -5.42978;
+
+    let point1 = new Point(1500, 1000)
+    let ball1 = new Ball2D(point1);
+
+    ball1.speed.vx = 2.1776212618576574;
+    ball1.speed.vy = 0.23529411764705885;
+
+    let point2 = new Point(1510, 1000)
+    let ball2 = new Ball2D(point2);
+
+    ball2.speed.vx = -6.42978;
+    ball2.speed.vy =  0.69627;
+
+
+
+    let balls: Array<Ball2D> = new Array();
+    balls.push(ball1);
+    balls.push(ball2);
+ 
+    service.checkCollisionsBetweenBallsAndUpdateVelocity(balls);
+
+    expect(balls[0].speed.vy).toEqual(expectedVyBall1);
+    expect(balls[0].speed.vx).toEqual(expectedVxBall1);
+    expect(balls[1].speed.vy).toEqual(expectedVyBall2);  
+    expect(balls[1].speed.vx).toEqual(expectedVxBall2);  
 
   });
 
